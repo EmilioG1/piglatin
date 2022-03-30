44581f0569e87e127e63a1c167e7f7155aebecaf
Emilio Gonzales <emiliog1184@gmail.com>
2022-03-30 15:27:00 -0700

    Pass test 16
    Co-authored-by: Cameron Ray <crayiphone@gmail.com>
    co-authored-by: Tony Martinez <tony.martinez5897@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,24 +1,26 @@
-// Test: "if a word begins with consonants, and qu is in the consecutive consonants, don't move the q with the consonants."
-// code: pigLatin("squeal");
-// expected output: "quealsay"
-
-function pigLatin(element) {
-  const vowel = ['a', 'e', 'i', 'o', 'u'];
-  const vowelQ = ['a', 'e', 'i', 'o', 'u', 'q'];
-  for (index = 0; index < element.length; index++) {
-    if (element[0] === vowel[index]) {
-      return element + "way";
-    } else if (element[0] === "q" && element[1] === "u") {
-      return element.slice(2, element.length) + "quay";
-    } else if (vowelQ.includes(element[index])) {
-      return element.slice(index, element.length) + element.slice(0, index) + "ay";
-    }
-  }
-  return false;
-} 
+// Test: "it should handle multiple words in string for all cases"
+// code: pigLatin("are you cooking quail if so we squeal")
+// expected output:"areway ouyay ookingcay ailquay ifway osay eway quealsay"
+function pigLatin(text) {
+  const words = text.trim().split(" ");
+  let latin = [];
+  words.forEach(function(element) {
+    const vowel = ['a', 'e', 'i', 'o', 'u'];
+    const vowelQ = ['a', 'e', 'i', 'o', 'u', 'q'];
+    for (index = 0; index < vowel.length; index++) {
+      if (vowel.includes(element[0])) {
+        return latin.push(element + "way");
+      } else if (element[0] === "q" && element[1] === "u") {
+        return latin.push(element.slice(2, element.length) + "quay");
+      } else if (vowelQ.includes(element[index])) {
+        return latin.push(element.slice(index, element.length) + element.slice(0, index) + "ay");
+      } 
+    } 
+   });
+  return latin.join(" ");
+}
 
 $(document).ready(function(){
-  console.log(pigLatin("quick"));
+  console.log(pigLatin("are you cooking quail if so we squeal"));
+
 });
-
-
