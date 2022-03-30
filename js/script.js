15e1956b7e039d9d3db03ffe14009f69c04a9d2b
Emilio Gonzales <emiliog1184@gmail.com>
2022-03-30 13:18:48 -0700

    Pass test 6
    Co-authored-by: Cameron Ray <crayiphone@gmail.com>
    co-authored-by: Tony Martinez <tony.martinez5897@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-// Test: "it should identify if the first letter of a word is a vowel"
+// Test: "it should identify if the first letter of a word is a vowel if so concat way to the end of the word"
 // code: pigLatin("ant");
-// Expected Output: true
+// Expect Output: antway
 
 function pigLatin(element) {
   const vowel = ['a', 'e', 'i', 'o', 'u'];
   for (index = 0; index < vowel.length; index++) {
-    if (element[0] === vowel[0]) {
-      return true;
+    if (element[0] === vowel[index]) {
+      return element + "way";
     }
   }
   return false;
 } 
 
-console.log(pigLatin("ant"));
+console.log(pigLatin("time"));
