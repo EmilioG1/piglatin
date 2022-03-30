7f897c1b4a604e5d8a2570d3dfde4aa8087cb437
Emilio Gonzales <emiliog1184@gmail.com>
2022-03-30 13:26:21 -0700

    Pass test 7
    Co-authored-by: Cameron Ray <crayiphone@gmail.com>
    co-authored-by: Tony Martinez <tony.martinez5897@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
-// Test: "it should identify if the first letter of a word is a vowel if so concat way to the end of the word"
-// code: pigLatin("ant");
-// Expect Output: antway
+// Test: "It should identify if the first letter of a word is a qu"
+// code: pigLatin("queen");
+// expected Output: true
 
 function pigLatin(element) {
   const vowel = ['a', 'e', 'i', 'o', 'u'];
   for (index = 0; index < vowel.length; index++) {
     if (element[0] === vowel[index]) {
       return element + "way";
+    } else if (element[0] === "q" && element[1] === "u") {
+      return true;
     }
   }
   return false;
 } 
 
-console.log(pigLatin("time"));
+console.log(pigLatin("squid"));
