fc69c2ef7795c7badbe6f749e4b81f0109a94771
Emilio Gonzales <emiliog1184@gmail.com>
2022-03-30 15:38:18 -0700

    Pass test 17
    Co-authored-by: Cameron Ray <crayiphone@gmail.com>
    co-authored-by: Tony Martinez <tony.martinez5897@gmail.com>

--- js/script.js
+++ js/script.js
@@ -5,22 +5,27 @@ function pigLatin(text) {
   const words = text.trim().split(" ");
   let latin = [];
   words.forEach(function(element) {
+    if (Number(element)){
+      latin.push(element);
+    } else {
     const vowel = ['a', 'e', 'i', 'o', 'u'];
     const vowelQ = ['a', 'e', 'i', 'o', 'u', 'q'];
     for (index = 0; index < vowel.length; index++) {
-      if (vowel.includes(element[0])) {
+      if (vowel.includes(element[0].toLowerCase())) {
         return latin.push(element + "way");
-      } else if (element[0] === "q" && element[1] === "u") {
+      } else if (element[0].toLowerCase() === "q" && element[1].toLowerCase() === "u") {
         return latin.push(element.slice(2, element.length) + "quay");
-      } else if (vowelQ.includes(element[index])) {
+      } else if (vowelQ.includes(element[index].toLowerCase())) {
         return latin.push(element.slice(index, element.length) + element.slice(0, index) + "ay");
       } 
     } 
+  }
    });
   return latin.join(" ");
-}
+  }
+
 
 $(document).ready(function(){
-  console.log(pigLatin("are you cooking quail if so we squeal"));
+  console.log(pigLatin("Are 2 you?"));
 
 });
