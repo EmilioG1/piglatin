d5549e9e7d4e7242341a5e8bbf12c1012a05805d
Emilio Gonzales <emiliog1184@gmail.com>
2022-03-30 13:37:13 -0700

    Pass test 9
    Co-authored-by: Cameron Ray <crayiphone@gmail.com>
    co-authored-by: Tony Martinez <tony.martinez5897@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-// Test: "It should identify if q and u are in a word."
-// code: pigLatin("squeal");
-// Expected Output: true
+// Test: "If a word starts with qu it should move q and u to the end of a word."
+// code: pigLatin("quick");
+// Expected Output: ("ickqu")
 
 function pigLatin(element) {
   const vowel = ['a', 'e', 'i', 'o', 'u'];
   for (index = 0; index < vowel.length; index++) {
     if (element[0] === vowel[index]) {
       return element + "way";
-    } else if (element[0] === "q" && element[1] === "u" || element.includes("qu")) {
-      return true;
+    } else if (element[0] === "q" && element[1] === "u") {
+      return element.slice(2, element.length) + "qu";
     }
   }
   return false;
 } 
 
-console.log(pigLatin("squit"));
+console.log(pigLatin("quilt"));
