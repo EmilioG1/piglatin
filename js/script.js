b9c601741f49db62a2bb8b810115aeaf0e1d1b58
Emilio Gonzales <emiliog1184@gmail.com>
2022-03-30 13:32:45 -0700

    Pass test 8
    Co-authored-by: Cameron Ray <crayiphone@gmail.com>
    co-authored-by: Tony Martinez <tony.martinez5897@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-// Test: "It should identify if the first letter of a word is a qu"
-// code: pigLatin("queen");
-// expected Output: true
+// Test: "It should identify if q and u are in a word."
+// code: pigLatin("squeal");
+// Expected Output: true
 
 function pigLatin(element) {
   const vowel = ['a', 'e', 'i', 'o', 'u'];
   for (index = 0; index < vowel.length; index++) {
     if (element[0] === vowel[index]) {
       return element + "way";
-    } else if (element[0] === "q" && element[1] === "u") {
+    } else if (element[0] === "q" && element[1] === "u" || element.includes("qu")) {
       return true;
     }
   }
   return false;
 } 
 
-console.log(pigLatin("squid"));
+console.log(pigLatin("squit"));
