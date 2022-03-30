a72bef8015d173b1cfca5a9e6e230f008c95787e
Emilio Gonzales <emiliog1184@gmail.com>
2022-03-30 14:45:37 -0700

    Pass test 15
    Co-authored-by: Cameron Ray <crayiphone@gmail.com>
    co-authored-by: Tony Martinez <tony.martinez5897@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,25 +1,24 @@
-// Test: "If a word begins with multiple consonants move them to the back of the word and concat "ay" to the end"
-// code: pigLatin("checkers")
-// expected output: "eckerschay"
+// Test: "if a word begins with consonants, and qu is in the consecutive consonants, don't move the q with the consonants."
+// code: pigLatin("squeal");
+// expected output: "quealsay"
 
 function pigLatin(element) {
   const vowel = ['a', 'e', 'i', 'o', 'u'];
+  const vowelQ = ['a', 'e', 'i', 'o', 'u', 'q'];
   for (index = 0; index < element.length; index++) {
     if (element[0] === vowel[index]) {
       return element + "way";
     } else if (element[0] === "q" && element[1] === "u") {
       return element.slice(2, element.length) + "quay";
-    } else if (vowel.includes(element[index])) {
+    } else if (vowelQ.includes(element[index])) {
       return element.slice(index, element.length) + element.slice(0, index) + "ay";
     }
   }
   return false;
 } 
 
-let string = "checkers";
-console.log(string[2]);
 $(document).ready(function(){
-  console.log(pigLatin("squeal"));
+  console.log(pigLatin("quick"));
 });
 
 
